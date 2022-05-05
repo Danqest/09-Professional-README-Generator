498c0e76527574c57ee03b3948f9abcf9a5a53fb
Colin Brooks <mcbrooks@pm.me>
2022-05-04 21:21:55 -0400

    updated index.js and added test readme

--- index.js
+++ index.js
@@ -5,116 +5,95 @@ inquirer
     {
         type: "input",
         name: "projectTitle",
-      //   message: "What is your name?",
+        message: "What is the project title?",
       },
     {
       type: "input",
       name: "description",
-    //   message: "What is your name?",
+      message: "Write a short description of the purpose of this project.",
     },
-    {
-        type: "input",
-        name: "tableOfContents",
-      //   message: "What is your name?",
-      },
     {
       type: "input",
       name: "installation",
-    //   message: "Where are you located?",
+      message: "Write a short set of instructions for installation of the project.",
     },
     {
       type: "input",
       name: "usage",
-    //   message: "Write a short Bio about yourself",
+      message: "Write a short set of instructions for how to use the project.",
       
     },
     {
       type: "input",
       name: "contributing",
-    //   message: "What is your GitHub URL",
+      message: "Write a short set of instructions for how to contribute to the project.",
     },
     {
       type: "input",
       name: "tests",
-    //   message: "What is your LinkedIn URL",
+      message: "Write a short set of instructions for how to test the project.",
     },
     {
         type: "input",
         name: "username",
-      //   message: "What is your LinkedIn URL",
-      },
-      {
-        type: "input",
-        name: "github",
-      //   message: "What is your LinkedIn URL",
+        message: "If people need to contact you for questions, what is your github username?",
       },
       {
         type: "input",
         name: "email",
-      //   message: "What is your LinkedIn URL",
+        message: "Additionally, what is your email in case people want to reach out to you directly?",
       },
       {
-        type: "input",
+        type: "rawlist",
         name: "license",
-      //   message: "Write a short Bio about yourself",
+        message: "Write license would you like to use with this README generator?",
+        choices: [
+          'MIT',
+          'Apache',
+          'GPL',
+          'BSD'
+        ]
         
       },
   ])
   .then((data) => {
     console.log(data);
     const readme = generateREADME(data)
-    const filename = `README.me`;
+    const filename = `README.md`;
     fs.writeFile(filename, readme, (err) =>
       err ? console.log(err) : console.log("Success!"))
     })
 
     function generateREADME(data) {
         return `# ${data.projectTitle}
+        ![license](https://img.shields.io/badge/${data.license}-License-green)
         DESCRIPTION:
         ${data.description}
 
         TABLE OF CONTENTS:
-        1) Installation
+        1) [INSTALLATION](#Installation)
         2) Usage
-        3) Contribution Guidelines
-        4) Test Instructions
+        3) Contributing
+        4) Tests
 
-        1) INSTALLATION:
+        #<a name="installation"></a> INSTALLATION:
         ${data.installation}
 
         2) USAGE:
         ${data.usage}
 
-        3) CONTRIBUTION GUIDELINES:
+        3) CONTRIBUTING:
         ${data.contributing}
 
-        4) TEST INSTRUCTIONS:
+        4) TESTS:
         ${data.tests}
 
         QUESTIONS - CONTACT:
-        Github: ${data.username}
+        Github: [![${data.username} Github](https://www.github.com/${data.username})]()
         Email: ${data.email}
 
         LICENSE:
-        ${data.license}
-
-          <meta charset="UTF-8">
-          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-          <title>Document</title>
-        </head>
-        <body>
-          <div class="jumbotron jumbotron-fluid">
-          <div class="container">
-            <h1 class="display-4">Hi! My name is ${data.name}</h1>
-            <p class="lead">I am from ${data.Location}.</p>
-            <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-            <ul class="list-group">
-              <li class="list-group-item">My GitHub username is ${data.gitHub}</li>
-              <li class="list-group-item">LinkedIn: <a href="${data.linkedIn}">${data.linkedIn}</a></li>
-            </ul>
-          </div>
-        </div>
-        </body>
-        </html>`
+        This application is covered under the ${data.license} License.
+        
+        `
     }
